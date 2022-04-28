5091d01cb313692e19e56537df8338be73f9fe69
Kyle O'Block <masamune17@rocketmail.com>
2022-04-27 22:01:45 -0700

    Finished // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript

---
+++ 8 Kyu/555086d53eac039a2a000083.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2){
+    // moment of truth
+    if((flower1 + flower2) % 2 == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
