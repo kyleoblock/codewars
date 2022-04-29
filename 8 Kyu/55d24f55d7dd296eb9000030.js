a815482ced41c591a8dbad7dc40bfe82a382cb8f
Kyle O'Block <kyleoblock@gmail.com>
2022-04-29 08:56:06 -0700

    Completed Grasshopper - Summation

---
+++ 8 Kyu/55d24f55d7dd296eb9000030.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+var summation = function (num) {
+    // Code here
+    let total = 0;
+    for(let i = 1; i <= num; i++) {
+      total += i;
+    }
+    return total;
+  }
