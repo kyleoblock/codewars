be6b5b27edf292284d78dfa306bb3534e3792c7d
Kyle O'Block <masamune17@rocketmail.com>
2022-05-01 19:59:38 -0700

    Finished 54edbc7200b811e956000556

---
+++ 7 Kyu/54edbc7200b811e956000556.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    let total = 0 ;
+    arrayOfSheep.forEach( element => {
+      if(element == true) {
+        total += 1;
+      }
+    })
+    return total;
+  }
