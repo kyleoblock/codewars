7fd052fc2b29bb87276cfbd529c8b9754713a956
Kyle O'Block <kyleoblock@gmail.com>
2022-05-05 22:55:43 -0700

    5/5 Codewar Solutions

---
+++ 8 Kyu/523b4ff7adca849afe000035.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
+// Write a function "greet" that returns "hello world!"
+function greet() {
+    return "hello world!";
+  }
+  
